1cd8aa37f3bc4b1ed586c6d34bcabab780bc6318
dimas indra <dimas.indra773@gmail.com>
2023-08-25 19:42:50 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import HomePages from "./Pages/HomePages/HomePages";
 import NavbarTongpung from "./Components/Navbar/NavbarTongpung";
 import Footer from "./Components/Footer/Footer";
-import MainContent from "./Components/MainContent/MainContent";
 
 function App() {
   return (
     <div className="App">
       <NavbarTongpung />
       <HomePages /> 
-      <MainContent />
       <Footer />
     </div>
   );
